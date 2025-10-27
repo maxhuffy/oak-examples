0dba12c2341076e25254668284d2c05f7317c20d
Dani Rogmans <danirogmans@gmail.com>
2025-10-27 12:39:45 +0100

    minimal example for now

--- neural-networks/generic-example/minimal.py
+++ neural-networks/generic-example/minimal.py
@@ -20,16 +20,18 @@ with dai.Pipeline(device) as pipeline:
     print("Creating pipeline...")
 
     cameraNode = pipeline.create(dai.node.Camera).build()
-    model_description = dai.NNModelDescription("luxonis-ml-team/bean-classification-dinov3:224-x-224-classification-head-classification-parser", platform=platform)
+    model_description = dai.NNModelDescription("luxonis-ml-team/bean-classification-dinov3:newest:948a6d5", platform=platform)
     nn_archive = dai.NNArchive(dai.getModelFromZoo(model_description))
     neuralNetwork = pipeline.create(dai.node.NeuralNetwork).build(cameraNode, nn_archive)
-
     qNNData = neuralNetwork.out.createOutputQueue()
 
     print("Pipeline created")
     pipeline.start()
-
+    print("Pipeline started")
 
     while pipeline.isRunning():
+        print("Pipeline is running")
         inNNData: dai.NNData = qNNData.get()
         tensor = inNNData.getFirstTensor()
+        assert (isinstance(tensor, np.ndarray))
+        print(f"Received NN data: {tensor.shape}")
